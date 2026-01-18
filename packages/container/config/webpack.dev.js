97fa40a992e546380adab045df9b69e271311713
Arpit <arpit@campeya.com>
2026-01-18 15:43:49 +0530

    Initial Commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,27 @@
+const {merge}=require('webpack-merge');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const ModulefederationPlugin=require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig=require('./webpack.common');
+const packageJson=require('../package.json');
+const devConfig={
+    mode:'development',
+    devServer:{
+        port:8080,
+        historyApiFallback:{
+            index:'index.html'
+        }
+    },
+    plugins:[
+       new ModulefederationPlugin({
+            filename:'Container',
+            remotes:{
+            marketing:'marketing@http://localhost:8081/remoteEntry.js'
+            },
+            shared:packageJson.dependencies,
+        }),
+        new HtmlWebpackPlugin({
+            template:'./public/index.html'
+        })
+    ]
+};
+module.exports=merge(commonConfig,devConfig);
